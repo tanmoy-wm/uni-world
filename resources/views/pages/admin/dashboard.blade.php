52652c08056504011ede06baafa4e4d6378def0b
tanmoy-wm <132640868+tanmoy-wm@users.noreply.github.com>
2023-05-12 16:03:45 +0530

    Updated: img

--- resources/views/pages/admin/dashboard.blade.php
+++ resources/views/pages/admin/dashboard.blade.php
@@ -12,7 +12,8 @@
         <div class="col-md-4 stretch-card grid-margin">
             <div class="card bg-gradient-danger card-img-holder text-white">
                 <div class="card-body">
-                    <img src="assets/images/dashboard/circle.svg" class="card-img-absolute" alt="circle-image" />
+                    <img src="{{ asset('assets/images/dashboard/circle.svg') }}" class="card-img-absolute"
+                        alt="circle-image" />
                     <h4 class="font-weight-normal mb-3">Weekly Sales <i class="mdi mdi-chart-line mdi-24px float-right"></i>
                     </h4>
                     <h2 class="mb-5">$ 15,0000</h2>
@@ -23,7 +24,8 @@
         <div class="col-md-4 stretch-card grid-margin">
             <div class="card bg-gradient-info card-img-holder text-white">
                 <div class="card-body">
-                    <img src="assets/images/dashboard/circle.svg" class="card-img-absolute" alt="circle-image" />
+                    <img src="{{ asset('assets/images/dashboard/circle.svg') }}" class="card-img-absolute"
+                        alt="circle-image" />
                     <h4 class="font-weight-normal mb-3">Weekly Orders <i
                             class="mdi mdi-bookmark-outline mdi-24px float-right"></i>
                     </h4>
@@ -35,7 +37,8 @@
         <div class="col-md-4 stretch-card grid-margin">
             <div class="card bg-gradient-success card-img-holder text-white">
                 <div class="card-body">
-                    <img src="assets/images/dashboard/circle.svg" class="card-img-absolute" alt="circle-image" />
+                    <img src="{{ asset('assets/images/dashboard/circle.svg') }}" class="card-img-absolute"
+                        alt="circle-image" />
                     <h4 class="font-weight-normal mb-3">Visitors Online <i class="mdi mdi-diamond mdi-24px float-right"></i>
                     </h4>
                     <h2 class="mb-5">95,5741</h2>
